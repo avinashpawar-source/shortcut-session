3d3fb00eb1482a82f7de7ad4aebf8e7694d6dd5f
avipawardev <avipawar.dev@gmail.com>
2026-09-22 12:26:15 +0530

    code initiated

---
+++ client/src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000/";
+
+const instance = axios.create({
+  baseURL: API_BASE,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  console.log(config)
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default instance;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const authRouter = require("./routes/authRoutes");
+const candidateRouter = require("./routes/candidateRoutes");
+const app = express();
+
+app.use(express.json());
+
+// CORS configuration
+const corsOptions = {
+  origin: "*", // Allow all origins (change to specific domain in production)
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use("/auth", authRouter);
+app.use("/candidates", candidateRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server started on port:${process.env.PORT || 3000}`);
+  connectDB();
+});
+
+
